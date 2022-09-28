53c3a3ebde1d65bc6c822f335a647da8dcc9de43
PabloC <7736022@alumnos.ufv.es>
2022-09-28 20:36:14 +0200

    HolaMundo creado

---
+++ src/com/ufv/holamundo/HolaMundo.java
@@ -0,0 +1,8 @@
+package com.ufv.holamundo;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo\n");
+        System.out.println("Hola Mundo\n");
+    }
+}
